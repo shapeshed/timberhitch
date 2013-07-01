60274f8d54ab8a6ced96ef0a5930de7820b768a9
George Ornbo <george@shapeshed.com>
2013-07-01 13:27:05 +0100

    [refactor] do not prescribe string

--- test/timberhitch-test.js
+++ test/timberhitch-test.js
@@ -6,14 +6,14 @@ describe('Using the timberhitch module', function () {
 
   describe('When not providing a component string', function () {
     it('should generate a logging key without the compoenent', function (done) {
-      assert.strictEqual(timberhitch(), '[' + os.hostname() + ':' + process.pid + '] ');
+      assert.strictEqual(timberhitch(), os.hostname() + ':' + process.pid);
       done();
     });
   });
 
   describe('When providing a component string', function () {
     it('should generate a logging key with the component', function (done) {
-      assert.strictEqual(timberhitch('myapp'), '[myapp' + ':' + os.hostname() + ':' + process.pid + '] ');
+      assert.strictEqual(timberhitch('myapp'), 'myapp' + ':' + os.hostname() + ':' + process.pid);
       done();
     });
   });
